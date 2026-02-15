ffd9ddb14628daf890f7b7b44c150efc3c48b9ce
TheFree-web <jeroendecaluw@gmail.com>
2026-02-16 00:26:41 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 // Voor toekomstige interactieve functies
 console.log("The Free website loaded");
 
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    // Toggle menu
+    if(navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
