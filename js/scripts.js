babdaecd703da6af2e812c2f70ad853ff60db0ad
TheFree-web <jeroendecaluw@gmail.com>
2026-02-16 07:57:40 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@
 console.log("The Free website loaded");
 
 document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
     const btnLuister = document.querySelector('.btn-luister');
     const audioPlayer = document.getElementById('audio-player');
 
