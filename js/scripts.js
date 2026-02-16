d56b4e611e2b6bdd52fda0f794dc1050fb8a1191
TheFree-web <jeroendecaluw@gmail.com>
2026-02-16 01:19:42 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 // Voor toekomstige interactieve functies
 console.log("The Free website loaded");
 
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-    // Toggle menu
-    if(navLinks.style.display === 'flex') {
-        navLinks.style.display = 'none';
-    } else {
-        navLinks.style.display = 'flex';
-    }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
+    // Hamburger menu
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+    // Luister nu knop
     const btnLuister = document.querySelector('.btn-luister');
     const audioPlayer = document.getElementById('audio-player');
 
